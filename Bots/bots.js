d69c052f18025fac98175455785618a9f4c479f1
naktheking <78290773+naktheking@users.noreply.github.com>
2025-08-06 16:54:26 -0500

    init git repo and made basic format

---
+++ Bots/bots.js
@@ -0,0 +1,8 @@
+function showBotCount() {
+    document.getElementById('bot-count').style.display = 'show';
+    
+}
+
+function hideBotCount() {
+    document.getElementById('bot-count').style.display = 'none';
+}
